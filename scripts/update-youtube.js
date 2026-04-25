8caaf2014ed72c48ef5be9c9046d1f202f2d4fa7
kill662477-cmyk <kill662477@gmail.com>
2026-04-25 14:17:19 +0900

    Update update-youtube.js

--- scripts/update-youtube.js
+++ scripts/update-youtube.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 const SOURCE_FILE = "data/youtube-sources.json";
 const OUTPUT_FILE = "youtube.json";
-const MAX_ITEMS = 10;
+const MAX_ITEMS = 9;
 
 function nowKST() {
   const date = new Date();
